b18c9f500f027592fbd27c57ab88a145318bb87a
Zhangjian He <hezhangjian97@gmail.com>
2026-09-17 00:12:30 +0800

    build: configure Cloudflare adapter deployment
    Signed-off-by: Zhangjian He <hezhangjian97@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://hezhangjian.com',
+
+  adapter: cloudflare(),
+});
