4716df60c82c84cd0ab2552fb6b87f63f347351d
Zhangjian He <hezhangjian97@gmail.com>
2026-09-18 17:38:11 +0800

    feat: centralize site layout metadata
    Signed-off-by: Zhangjian He <hezhangjian97@gmail.com>

--- src/config/content.mjs
+++ src/config/content.mjs
@@ -2,3 +2,4 @@ import { homedir } from 'node:os';
 import { resolve } from 'node:path';
 
 export const BLOG_CONTENT_DIR = resolve(homedir(), 'OneDrive/Blog');
+export const PRESENTATIONS_CONTENT_DIR = resolve(homedir(), 'OneDrive/Presentations');
